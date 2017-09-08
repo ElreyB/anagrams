b86a13ddafc80d71783829e3a7dfee84b344a0d7
Elrey Belmonti <elbelmonti@gmail.com>
2017-09-08 09:44:21 -0700

    return confirmation phrase

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,6 +9,14 @@ class Anagram
     @alphabetical_form ||= alphabetically_sorted
   end
 
+  def anagram_of(possible_anagram)
+    if self.alphabetical_form == possible_anagram.alphabetical_form
+      return "These words are anagrams."
+    end
+      "These words are not anagrams"
+  end
+
+private
   def alphabetically_sorted
     word.split("").sort.join
   end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,15 +2,27 @@ require 'rspec'
 require 'anagram'
 
 describe 'Anagram' do
-  let(:subject) { Anagram.new("ruby") }
+  let(:word1) { Anagram.new("ruby") }
 
   it 'has a readable word' do
-    expect(subject.word).to eq "ruby"
+    expect(word1.word).to eq "ruby"
   end
 
   describe '#alphabetical_form' do
     it 'returns the word sorted alphabetically' do
-      expect(subject.alphabetical_form).to eq "bruy"
+      expect(word1.alphabetical_form).to eq "bruy"
+    end
+  end
+
+  describe 'identifying an anagram' do
+    it 'returns confirm phrase if alphabetical forms are equal' do
+      word2 = Anagram.new("bruy")
+      expect(word1.anagram_of(word2)).to eq "These words are anagrams."
+    end
+
+    it 'returns disconfirm phrase if alphabetical forms are not equal' do
+      word2 = Anagram.new("snow")
+      expect(word1.anagram_of(word2)).to eq "These words are not anagrams"
     end
   end
 end
